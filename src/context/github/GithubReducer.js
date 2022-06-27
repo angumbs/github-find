0a9c7b826211ccec12c8311fd9ea8b8d2b53e595
Angela <angela.n.gumbs@gmail.com>
2022-06-27 09:50:27 -0400

    initial commit

---
+++ src/context/github/GithubReducer.js
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+const githubReducer = (state, action) => {
+switch (action.type) {
+    case 'GET_USERS':
+        return {
+            ...state,
+            users: action.payload,
+            loading: false,
+        }
+    case 'SET_LOADING':
+        return {
+            ...state,
+            loading: true
+        }
+    default:
+        return useState
+}
+}
+
+export default githubReducer
